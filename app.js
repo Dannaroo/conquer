f8411ec4a8f083d431fb13f5491f79347001a470
Danny Ward <liege88@gmail.com>
2018-10-06 21:40:42 +0800

    fix navbar button spacing and inconsistent sizing between buttons. fixed return to top arrow fading in/out at wrong spot by preventDefault() on a tags. Highlighting nav links based on which section is being viewed and also fixed nav button hightling when hovering and when transition between sections.

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 
 const button = document.getElementsByClassName("reveal-nav");
 const navBar = document.getElementsByClassName("main-nav");
+const navParent = document.querySelector('#navParent');
+const children = navParent.children;
+var navArr = Array.from(children);
 
+// add nav button functionality
 button[0].addEventListener('click', () => {
   if (navBar[0].style.display === "flex") {
     navBar[0].style.display = "none";
@@ -11,6 +15,19 @@ button[0].addEventListener('click', () => {
     navBar[0].style.display = "flex";
   }
 });
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        navBar[0].style.display = "flex";
+    } else {
+        navBar[0].style.display = "none";
+    }
+}
+
+var x = window.matchMedia("(min-width: 767px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
 
 function backToTopArrow() {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
@@ -26,36 +43,64 @@ function backToTopArrow() {
 
 window.onscroll = function() {backToTopArrow()};
 
+// on hovering nav links, color them accordingly.
+// navArr.forEach(li => {
+//   li.firstElementChild.addEventListener('mouseover', (e) => {
+//     e.target.style.backgroundColor ="darkgrey";
+//   });
+//   li.firstElementChild.addEventListener('mouseout', (e) => {
+//     e.target.style.backgroundColor ="#36383a";
+//   });
+//
+//   li.firstElementChild.addEventListener('click', (e) => {
+//     navArr.forEach(li => li.firstElementChild.style.backgroundColor = "inherit");
+//       e.target.style.backgroundColor = "darkgrey";
+//   });
+// });
+
 //smooth scrolling
 
 $("#arrow").click(function() {
+
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#section1").offset().top
     }, 2000);
 });
 
-$("#homepageButton").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#section1").offset().top
-    }, 2000);
+$("#homepageButton").click(function(e) {
+  e.preventDefault();
+  navArr.forEach(li => li.firstElementChild.className = "dark-grey hover");
+  e.target.className = "clicked";
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#section1").offset().top
+  }, 2000);
 });
 
-$("#aboutusButton").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#section2").offset().top
-    }, 2000);
+$("#aboutusButton").click(function(e) {
+    e.preventDefault();
+    navArr.forEach(li => li.firstElementChild.className = "dark-grey hover");
+    e.target.className = "clicked";
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#section2").offset().top - 70
+  }, 2000);
 });
 
-$("#servicesButton").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#services").offset().top
-    }, 2000);
+$("#servicesButton").click(function(e) {
+    e.preventDefault();
+    navArr.forEach(li => li.firstElementChild.className = "dark-grey hover");
+    e.target.className = "clicked";
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#services").offset().top - 70
+  }, 2000);
 });
 
-$("#contactButton").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#contact").offset().top
-    }, 2000);
+$("#contactButton").click(function(e) {
+    e.preventDefault();
+    navArr.forEach(li => li.firstElementChild.className = "dark-grey hover");
+    e.target.className = "clicked";
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#contact").offset().top - 70
+  }, 2000);
 });
 
 //end smooth scrollTop
@@ -67,33 +112,31 @@ $("#contactButton").click(function() {
 // function(){
 //     $(this).removeClass("highlight").fadeIn("slow");
 // });
-
-$(".main-nav a").hover(
-function() {
-$(this).stop().animate({"background-color": "pink"}, "slow");
-console.log('1');
-},
-function() {
-$(this).stop().animate({"background-color": "blue)"}, "slow");
-console.log('2');
-});
+//
+// $(".main-nav a").hover(
+// function() {
+// $(this).stop().animate({"background-color": "pink"}, "slow");
+// },
+// function() {
+// $(this).stop().animate({"background-color": "blue)"}, "slow");
+// });
 
 // });
 
 //highlight on window position
-$(window).scroll(function() {
-    var windscroll = $(window).scrollTop();
-    if (windscroll) {
-        $('.scroll-section').each(function(i) {
-            if ($(this).position().top <= windscroll + 250) {
-                $('nav li a.active').removeClass('active');
-                $('nav li a').eq(i).addClass('active');
-            }
-        });
-
-    } else {
-        $('nav li a.active').removeClass('active');
-        $('nav li a:first').addClass('active');
-    }
-
-}).scroll();
+// $(window).scroll(function() {
+//     var windscroll = $(window).scrollTop();
+//     if (windscroll) {
+//         $('.scroll-section').each(function(i) {
+//             if ($(this).position().top <= windscroll + 250) {
+//                 $('nav li a.active').removeClass('active');
+//                 $('nav li a').eq(i).addClass('active');
+//             }
+//         });
+//
+//     } else {
+//         $('nav li a.active').removeClass('active');
+//         $('nav li a:first').addClass('active');
+//     }
+//
+// }).scroll();
